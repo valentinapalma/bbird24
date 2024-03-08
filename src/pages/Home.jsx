3eb19031801a0cfb16f7fb6c948bc0f43af9b9a9
Valentina <valentinapalma@hotmail.se>
2024-03-08 11:11:39 +0100

    SETUP
    Setup react project with images and folders

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+    return (
+      <div className="container">
+        <div className="row">
+            <div className="col">
+                <h1>Blackbird recruitment</h1>
+            </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Home;
